2baeadb6cb97a832849af89e38dacc44b6b138f5
Luis.Ruivo <Luis.Ruivo@apprentice.ua92.ac.uk>
2023-05-23 15:53:36 +0100

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  apps: [
-    {
-      script: "npm start",
-    },
-  ],
+  apps: [{
+      script: "npm start"
+    }],
 
   deploy: {
     production: {
@@ -14,10 +12,9 @@ module.exports = {
       repo: "git@github.com:LuisRuivo21/my-project.git",
       path: "/home/ubuntu",
       "pre-deploy-local": "",
-      "post-deploy":
-        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "post-deploy": "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
       "pre-setup": "",
-      ssh_options: "ForwardAgent=yes",
-    },
-  },
+      ssh_options: "ForwardAgent=yes"
+    }
+  }
 };
